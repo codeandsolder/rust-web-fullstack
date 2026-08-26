b27854c00e3267f06be8a8a77390dcbebca45904
Code and Solder <codeandsolder@gmail.com>
2026-08-27 00:51:41 +0200

    remove duplicate service-local migrations

--- live-search/migrations/001_create_search_results.down.sql
+++
@@ -1,5 +0,0 @@
-DROP TRIGGER IF EXISTS trg_search_result_insert ON search_results;
-DROP TRIGGER IF EXISTS trg_search_result_fts ON search_results;
-DROP FUNCTION IF EXISTS notify_search_result();
-DROP FUNCTION IF EXISTS auto_update_fts();
-DROP TABLE IF EXISTS search_results;
